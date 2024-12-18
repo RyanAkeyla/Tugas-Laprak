23cff9f80bb3006a3cc0bd03a1e85fab726ef165
RyanAkeyla <ryanakeylanoviantowidodo@gmail.com>
2024-12-18 21:48:40 +0700

    Add files via upload

---
+++ praktikum9/latihansoal1/latihansoal1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bilangan int
+	fmt.Scan(&bilangan)
+	jumlahDigit := 0
+
+	for {
+		jumlahDigit++
+		bilangan /= 10
+		if bilangan == 0 {
+			break
+		}
+	}
+	fmt.Print(jumlahDigit)
+}
